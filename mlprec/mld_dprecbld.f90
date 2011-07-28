32d5a19a64e6dcf5931653000abc0aace6a3992b
Salvatore Filippone <salvatore.filippone@cranfield.ac.uk>
2011-07-28 09:21:19 +0000

    mld2p4-typevect:  mlprec/mld_c_as_smoother.f90  mlprec/mld_c_dec_map_bld.F90  mlprec/mld_c_diag_solver.f90  mlprec/mld_c_id_solver.f90  mlprec/mld_c_ilu_solver.f90  mlprec/mld_c_jac_smoother.f90  mlprec/mld_c_prec_type.f90  mlprec/mld_c_slu_solver.f90  mlprec/mld_caggrmap_bld.f90  mlprec/mld_caggrmat_asb.f90  mlprec/mld_caggrmat_nosmth_asb.F90  mlprec/mld_caggrmat_smth_asb.F90  mlprec/mld_ccoarse_bld.f90  mlprec/mld_cmlprec_aply.f90  mlprec/mld_cmlprec_bld.f90  mlprec/mld_cprecaply.f90  mlprec/mld_cprecbld.f90  mlprec/mld_cslu_bld.f90  mlprec/mld_cslud_bld.f90  mlprec/mld_csp_renum.f90  mlprec/mld_cumf_bld.f90  mlprec/mld_d_as_smoother.f90  mlprec/mld_d_dec_map_bld.F90  mlprec/mld_d_diag_solver.f90  mlprec/mld_d_id_solver.f90  mlprec/mld_d_ilu_solver.f90  mlprec/mld_d_jac_smoother.f90  mlprec/mld_d_prec_type.f90  mlprec/mld_d_slu_solver.f90  mlprec/mld_d_sludist_solver.f90  mlprec/mld_d_umf_solver.f90  mlprec/mld_daggrmap_bld.f90  mlprec/mld_daggrmat_asb.f90  mlprec/mld_daggrmat_minnrg_asb.F90  mlprec/mld_daggrmat_nosmth_asb.F90  mlprec/mld_daggrmat_smth_asb.F90  mlprec/mld_dcoarse_bld.f90  mlprec/mld_dmlprec_aply.f90  mlprec/mld_dmlprec_bld.f90  mlprec/mld_dprecaply.f90  mlprec/mld_dprecbld.f90  mlprec/mld_dslu_bld.f90  mlprec/mld_dslud_bld.f90  mlprec/mld_dsp_renum.f90  mlprec/mld_s_as_smoother.f90  mlprec/mld_s_dec_map_bld.F90  mlprec/mld_s_diag_solver.f90  mlprec/mld_s_id_solver.f90  mlprec/mld_s_ilu_solver.f90  mlprec/mld_s_jac_smoother.f90  mlprec/mld_s_prec_type.f90  mlprec/mld_s_slu_solver.f90  mlprec/mld_saggrmap_bld.f90  mlprec/mld_saggrmat_asb.f90  mlprec/mld_saggrmat_nosmth_asb.F90  mlprec/mld_saggrmat_smth_asb.F90  mlprec/mld_scoarse_bld.f90  mlprec/mld_smlprec_aply.f90  mlprec/mld_smlprec_bld.f90  mlprec/mld_sprecaply.f90  mlprec/mld_sprecbld.f90  mlprec/mld_sslu_bld.f90  mlprec/mld_sslud_bld.f90  mlprec/mld_ssp_renum.f90  mlprec/mld_sumf_bld.f90  mlprec/mld_z_as_smoother.f90  mlprec/mld_z_dec_map_bld.F90  mlprec/mld_z_diag_solver.f90  mlprec/mld_z_id_solver.f90  mlprec/mld_z_ilu_solver.f90  mlprec/mld_z_jac_smoother.f90  mlprec/mld_z_prec_type.f90  mlprec/mld_z_slu_solver.f90  mlprec/mld_z_umf_solver.f90  mlprec/mld_zaggrmap_bld.f90  mlprec/mld_zaggrmat_asb.f90  mlprec/mld_zaggrmat_nosmth_asb.F90  mlprec/mld_zaggrmat_smth_asb.F90  mlprec/mld_zas_aply.f90  mlprec/mld_zas_bld.f90  mlprec/mld_zbaseprec_aply.f90  mlprec/mld_zbaseprec_bld.f90  mlprec/mld_zcoarse_bld.f90  mlprec/mld_zdiag_bld.f90  mlprec/mld_zfact_bld.f90  mlprec/mld_zilu_bld.f90  mlprec/mld_zmlprec_aply.f90  mlprec/mld_zmlprec_bld.f90  mlprec/mld_zprecaply.f90  mlprec/mld_zprecbld.f90  mlprec/mld_zslu_bld.f90  mlprec/mld_zslud_bld.f90  mlprec/mld_zsp_renum.f90  mlprec/mld_zumf_bld.f90  tests/pdegen/runs/ppde.inp
    calls into DESC methods.

--- mlprec/mld_dprecbld.f90
+++ mlprec/mld_dprecbld.f90
@@ -95,7 +95,7 @@ subroutine mld_dprecbld(a,desc_a,p,info,amold,vmold)
   name = 'mld_dprecbld'
   info = psb_success_
   int_err(1) = 0
-  ictxt = psb_cd_get_context(desc_a)
+  ictxt = desc_a%get_context()
   call psb_info(ictxt, me, np)
   p%ictxt = ictxt
 
